03dd964ccff5fa53c086b1ccf5bd274a24708223
MadonnaMettias <madonnabartygoher@gmail.com>
2023-04-24 07:26:46 -0700

    first

--- _printf.c
+++ _printf.c
@@ -1,120 +1,59 @@
 #include "main.h"
 
 /**
- * _putchar - writes a character to stdout
- * @c: the character to print
- *
- * Return: 1 on success, -1 on error
- */
-int _putchar(char c)
-{
-	return (write(STDOUT_FILENO, &c, 1));
-}
-
-
-/**
- *print_num - a function that print an int.
- *@n: the int to print
- *Return: the numbers of charachter printed
- */
-int print_num(int n)
-{
-	int count = 0;
-
-	if (n / 10)
-		count += print_num(n / 10);
-	_putchar((n % 10) + '0');
-	count++;
-	return (count);
-}
-
-
-/**
- * print_bin - a function that print an int in binary.
- * @n: the int to print
- * Return: the number of char printed
- */
-int print_bin(unsigned int n)
-{
-	int count = 0;
-
-	if (n / 2)
-		count += print_bin(n / 2);
-	_putchar((n % 2) + '0');
-	count++;
-	return (count);
-}
-/**
- * _printf - prints output according to a format
+ * _printf - a function that prints output according to a format
  * @format: the format string
- *
- * Return: the number of characters printed (excluding the null byte)
+ * Return: the numbers of char printed without the null byte.
  */
 int _printf(const char *format, ...)
 {
+	int count = 0;
 	va_list args;
 
-	int count = 0;
-
 	va_start(args, format);
+
 	while (*format)
 	{
 		if (*format == '%')
 		{
-			format++;
-			if (*format == 'c')
+			switch (*++format)
 			{
-				char c = (char) va_arg(args, int);
-				_putchar(c);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-				while (*s)
-				{
-					_putchar(*s);
-					count++;
-					s++;
-				}
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int n = va_arg(args, int);
+				case 'c':
+					{
+						char c = va_arg(args, int);
 
-				if (n < 0)
-				{
-					_putchar('-');
-					count++;
-					n = -n;
-				}	
-				count += print_num(n);
-			}
-			else if (*format == 'b')
-			{
-				unsigned int n = va_arg(args, unsigned int);
-				count += print_bin(n);
-			}
-			else if (*format == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else
-			{
-				_putchar('%');
-				_putchar(*format);
-				count += 2;
+						write(1, &c, 1);
+						count++;
+						break;
+					}
+				case 's':
+					{
+						char *s = va_arg(args, char *);
+
+						write(1, s, strlen(s));
+						count += strlen(s);
+						break;
+					}
+				case '%':
+					{
+						write(1, "%", 1);
+						count++;
+						break;
+					}
+				default:
+					{
+						break;
+					}
 			}
 		}
 		else
 		{
-			_putchar(*format);
+			write(1, format, 1);
 			count++;
 		}
 		format++;
 	}
-
 	va_end(args);
 	return (count);
 }
+
--- main.h
+++ main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 int _putchar(char c);
 int print_num(int n);
